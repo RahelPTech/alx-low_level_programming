c0b916e656bd2070f44651b8259885ace4e99b15
RahelPTech <rahel@persistent-tech.com>
2022-07-04 10:46:55 -0700

    assign a random number to the variable n each time it is executed and prints out postive, negative or zero according to a condition

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,8 @@
 
 /**
 *main -> assign a random number to the variable n each time it is executed
-*and print outbased a condition
-*Return : always 0
+*and print out based a condition
+*return : always 0
 */
 int main(void)
 {
@@ -16,7 +16,9 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 	printf("%d is positive\n", n);
 if (n == 0)
+{
 	printf("%d is zero\n", n);
+}
 if (n < 0)
 {
 	printf("%d is negative\n", n);
