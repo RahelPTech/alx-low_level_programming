2d2ca3b9b45a8a61b49a0f9174b19652f31a9a8b
RahelPTech <rahel@persistent-tech.com>
2022-07-06 09:04:54 -0700

    C Nested loops print alphabets in order

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ *
+ *  Return: 0 success
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
+
+
