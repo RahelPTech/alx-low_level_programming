df4b170765e77c2ee772502a7608efdd97ec66f7
RahelPTech <rahel@persistent-tech.com>
2022-07-06 10:53:38 -0700

    C nested loop print in lowercase alphabit order 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabe_x10 -> prints the lower case alphabet 10 times
+ *
+ *  Return: 0 success
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (j = 'a' j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
+	}
+}
