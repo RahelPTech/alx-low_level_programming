69cd6d9783529529ef6ad81b1cf727cd36b694ae
RahelPTech <rahel@persistent-tech.com>
2022-10-14 23:46:50 -0700

    C if ALX

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,26 +3,29 @@
 #include <stdio.h>
 
 /**
-*main -> assign a random number to the variable n each time it is executed
-*and print out based a condition
-*return : always 0
-*/
-int main(void) {
-int n;
+ * main - block
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-	printf("%d is positive\n", n);
-if (n == 0)
-{
-	printf("%d is zero\n", n);
-}
-if (n < 0)
-{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n < 0)
+	{
 	printf("%d is negative\n", n);
-}
-return 0;
-}
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is positive\n", n);
+	}
 
-
+	return (0);
+}
