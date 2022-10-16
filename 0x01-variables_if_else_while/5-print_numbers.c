73c6a0ecd3c38059d598ffdb0284b97db036c443
RahelPTech <rahel@persistent-tech.com>
2022-10-15 18:18:45 -0700

    Print number sequences

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Print all single digit numbers of base 10 starting from 0.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nu;
+
+	for (nu = 0; nu < 10; nu++)
+		printf("%d", nu);
+
+	printf("\n");
+
+	return (0);
+}
