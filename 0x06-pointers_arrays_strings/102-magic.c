4d19eb74e27b212f9475f2b9cfe5cfd139693ffb
RahelPTech <rahel@persistent-tech.com>
2022-10-27 09:09:00 -0700

    No Magic work hard and understand

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-
+/*
+ * main - prints out array two to 98 folloed by new line
+ * @n: parameter one
+ * @a: an array of five
+ * @p: pointer parameter
+ * returns 0
+ */
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
-	
+
 	a[2] = 1024;
 	p = &n;
 /*
@@ -19,5 +25,5 @@ int main(void)
 	*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-       	return (0);
+	return (0);
 }
