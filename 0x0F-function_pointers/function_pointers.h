fa197887e9f228dba8b4cedf0a810e576af1631c
RahelPTech <rahel@persistent-tech.com>
2022-11-16 12:43:56 -0800

    0x0F. C - Function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,9 +2,17 @@
 #define _FUNCTION_POINTERS_H_
 
 /**
- * File: main
+ * File: function_pointers.h
  *
- * Desc: header file
+ * AUth: Brennan D Baraban
+ *
+ * Desc: Header file containing prototypes for all functions
+ *
+ * used in the 0x0E-function_pointers directory.
+ *
+ * @name: name of the person
+ *
+ * @f: is pointer
  */
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
