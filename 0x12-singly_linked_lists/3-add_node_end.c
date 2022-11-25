036eb46eaca4eafb5c28ed50aa2c81292dafdd81
RahelPTech <rahel@persistent-tech.com>
2022-11-25 14:46:07 -0800

    a function that adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,23 +1,15 @@
-/**
- * File: 3-add_node_end.c
- *
- * Auth: Brennan D Baraban
- */
 #include "lists.h"
-#include <string.h>
-
+#include "string.h"
 /**
- * add_node_end - Adds a new node at the end
+ * add_node_end - Adds a new node at the end of a list
  *
- * of a list_t list.
+ * @head: pointer to the head of the list
  *
- *  @head: A pointer the head of the list_t list.
+ * @str: string to be added to the list
  *
- *  @str: The string to be added to the list_t list.
+ * Return: NULL in case of failure
  *
- *  Return: If the function fails - NULL.
- *
- *  Otherwise - the address of the new element.
+ * or address or the element
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -31,7 +23,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	return (NULL);
 	dup = strdup(str);
 	if (str == NULL)
+	{
+	free(new);
 	return (NULL);
+	}
 	for (len = 0; str[len];)
 	len++;
 	new->str = dup;
